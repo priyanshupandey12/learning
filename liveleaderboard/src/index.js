cf5eb57ac26d7754017a7a140f5961713e99ae3e
priyanshupandey12 <priyanshupandat32@gmail.com>
2026-05-25 03:56:40 +0530

    feat: initialize Node.js application with Redis and Express
    - Add package.json with dependencies for express and ioredis
    - Create .gitignore to exclude node_modules
    - Implement article data structure in article.js
    - Set up user data structure in user.js
    - Develop API endpoints for viewing articles and managing leaderboard scores
    - Connect to Redis for storing and retrieving view counts and leaderboard scores

---
+++ liveleaderboard/src/index.js
@@ -0,0 +1,132 @@
+import express from 'express';
+import Redis from 'ioredis';
+import {article} from './article.js';
+import {user} from './user.js';
+
+
+const app = express();
+app.use(express.json());
+
+
+// Connect to Redis
+const redis = new Redis("redis://localhost:6380");
+
+
+app.post('/post/:id/view',async(req,res)=>{
+    const articleId=req.params.id;
+    const views= await article.find((article)=>article.id===parseInt(articleId))
+    if(views){
+        const viewCount = await redis.incr(`article:${articleId}:views`);
+        res.json({ views: viewCount });
+    } else {
+        res.status(404).json({ error: "Article not found" });
+    }
+});
+
+app.get('/post/:id/views',async(req,res)=>{
+    const articleId=req.params.id;
+    const views= await article.find((article)=>article.id===parseInt(articleId))
+    if(views){
+        const viewCount = await redis.get(`article:${articleId}:views`);
+        res.json({ views: parseInt(viewCount) || 0 ,article:views});
+    } else {
+        res.status(404).json({ error: "Article not found" });
+    }
+});
+
+
+app.post('/leaderboard/:id/score', async (req, res) => {
+
+    const userId = parseInt(req.params.id);
+
+    const { score } = req.body;
+
+   
+    const existingUser = user.find(
+        (u) => u.id === userId
+    );
+
+    if (!existingUser) {
+
+        return res.status(404).json({
+            error: "User not found"
+        });
+
+    }
+
+   
+    const updatedScore = await redis.zincrby(
+        "leaderboard",
+        score,
+        existingUser.name
+    );
+
+    res.json({
+        message: "Score updated",
+        user: existingUser.name,
+        updatedScore
+    });
+
+});
+
+app.get('/leaderboard', async (req, res) => {
+
+    const topUsers = await redis.zrevrange(
+        "leaderboard",
+        0,
+        9,
+        "WITHSCORES"
+    );
+
+    const leaderboard = [];
+
+    for (let i = 0; i < topUsers.length; i += 2) {
+
+        leaderboard.push({
+            userId: topUsers[i],
+            score: Number(topUsers[i + 1])
+        });
+
+    }
+
+    res.json(leaderboard);
+
+});
+
+app.get('/leaderboard/:id/rank', async (req, res) => {
+
+    const userId = parseInt(req.params.id);
+
+    const user = user.find((u) => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({
+            error: "User not found"
+        });
+    }
+
+    const rank = await redis.zrevrank(
+        "leaderboard",
+        user.name
+    );
+
+    if (rank === null) {
+
+        return res.status(404).json({
+            error: "User not found"
+        });
+
+    }
+
+    res.json({
+        user: user.name,
+        rank: rank + 1
+    });
+
+});
+
+
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+})
